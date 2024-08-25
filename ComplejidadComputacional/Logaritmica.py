499b1ebbfec0eedf4570ec11fe79e559379181c9
Juan Fe <165521278+juanfedevmaster@users.noreply.github.com>
2024-08-25 18:28:18 -0500

    Complejidad computacional
    Complejidad computacional

---
+++ ComplejidadComputacional/Logaritmica.py
@@ -0,0 +1,10 @@
+def ciclo_logaritmico():
+    n = 64  # O(1)
+    i = 0  # O(1)
+    k = 0  # O(1)
+    while (n > 1):  # O(log_{2}(n) + 1)
+        n = n / 2  # O(log_{2}(n))
+        k = k + 1  # O(log_{2}(n))
+
+    print(f"n:{n}, k:{k}")  # O(1)
+
